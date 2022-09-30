a482b19fb4549f434d6d71ad1192f8893780be8d
kerwindows <kerwindows@hotmail.com>
2022-09-30 12:10:45 -0400

    added useStates

---
+++ build/precache-manifest.11245e0fb9393caedbbdcfd8a285e315.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af61b7dff1de41beb003d69e5dc2b43c",
+    "url": "/react-eventHandling-v1/index.html"
+  },
+  {
+    "revision": "2b41c5feee3657d52e8d",
+    "url": "/react-eventHandling-v1/static/js/2.5fbbfc9f.chunk.js"
+  },
+  {
+    "revision": "49accf58c19dcf21bc4e",
+    "url": "/react-eventHandling-v1/static/js/main.2d8434fc.chunk.js"
+  },
+  {
+    "revision": "60efdaeda14a1cda8e5c",
+    "url": "/react-eventHandling-v1/static/js/runtime-main.d2e5bf85.js"
+  }
+]);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,6 @@
 import React, {useState} from "react";
 
-
 function App() {
-  
-
 const [headingText, setHeadingText] = useState("Hello")
 const [isMouseOver, setMouseOver] = useState(false)
 
