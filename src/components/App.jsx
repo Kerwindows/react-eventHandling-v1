59cb941dfd03b8eb72751c96e4ef0f9d27f499b6
Kerwin Thompson <kerwindows@hotmail.com>
2022-09-30 15:07:58 +0000

    Updated 2 files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+const [headingText, setHeadingText] = useState("Hello");
+const [backgroundColor, setBackgroundColor] = useState({
+  backgroundColor: "white",
+});
+
+const handleMouseOver = () => {
+  setBackgroundColor("black");
+};
+
+const handleMouseOut = () => {
+  setBackgroundColor("White");
+};
+
+const handleSubmit = () => {
+  setHeadingText("Submitted");
+};
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={backgroundColor}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
     </div>
   );
 }
